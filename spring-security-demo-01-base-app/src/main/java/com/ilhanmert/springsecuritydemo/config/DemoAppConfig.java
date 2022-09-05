a7a45b326e41aef0a7de7488146f0d0aa1251445
ilhanmert <ilhanmertalan@gmail.com>
2022-09-05 23:46:56 +0300

    Initial commit

---
+++ spring-security-demo-01-base-app/src/main/java/com/ilhanmert/springsecuritydemo/config/DemoAppConfig.java
@@ -0,0 +1,26 @@
+package com.ilhanmert.springsecuritydemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.ilhanmert.springsecuritydemo")
+public class DemoAppConfig {
+
+    // define a bean for viewResolver
+    @Bean
+    public ViewResolver viewResolver(){
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+
+        viewResolver.setPrefix("/WEB-INF/view/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+}
